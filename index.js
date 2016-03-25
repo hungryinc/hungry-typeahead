3691e9cebbfa09aab7f66b3e8389a9ad6b0e76a5
Zachary Blank <Zachary.Blank@akqa.com>
2016-03-25 09:10:20 -0700

    Require fs

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
+var fs = require('fs')
+
 angular.module('hgTypeahead', [])
 
-.directive('hungryTypeahead', ['$compile', '$q', function($compile, $q) {
-    var fs = require('fs')
-    
+.directive('hungryTypeahead', ['$compile', '$q', function($compile, $q) {    
     return {
         restrict: 'A',
         scope: {
